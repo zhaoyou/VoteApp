9cee7c2675d7926a5f7cd61d5ae084ee6e306286
hadeser <hadeser@eprotime.com>
2013-09-24 21:52:26 +0800

    update tempalte0923

--- client/template.js
+++ client/template.js
@@ -21,6 +21,35 @@ Template.index.show = function() {
   return ifViewing("index")
 }
 
+Template.index.rendered = function() {
+  $(function() {
+    $(".contrast .czsbg1").hide();
+    $(".contrast .czsbg2").hide();
+    $(".contrast .czsbg3").hide();
+    $(".contrast .czsbg4").hide();
+    $(".contrast .caizhuang1").mouseover(function() {
+      $(".contrast .czsbg1").show();
+    }).mouseout(function() {
+      $(".contrast .czsbg1").hide();
+    })
+    $(".contrast .caizhuang2").mouseover(function() {
+      $(".contrast .czsbg2").show();
+    }).mouseout(function() {
+      $(".contrast .czsbg2").hide();
+    })
+    $(".contrast .caizhuang3").mouseover(function() {
+      $(".contrast .czsbg3").show();
+    }).mouseout(function() {
+      $(".contrast .czsbg3").hide();
+    })
+    $(".contrast .caizhuang4").mouseover(function() {
+      $(".contrast .czsbg4").show();
+    }).mouseout(function() {
+      $(".contrast .czsbg4").hide();
+    })
+  })
+}
+
 Template.index2.show = function() {
   return ifViewing("index2")
 }
@@ -104,7 +133,7 @@ Template.index3.rendered = function() {
             }
         });
 
-    })
+    });
 
     $(function(){
         GaAgent.indexPage();
